6999049430f9f385db9e455061f50f729bf040bb
pbasiak <wilczek241@gmail.com>
2020-10-06 18:00:12 +0200

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5703a61eb84b96755d78683fab7a209f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '68175b03-751f-4c8f-bab9-49f885a13552'
+};
