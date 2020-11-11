af4345077b55274ec2e6327e1d0c54266037d91b
pbasiak <wilczek241@gmail.com>
2020-11-11 12:20:44 +0100

    databse prod

--- config/database.js
+++ config/database.js
@@ -1,18 +1,41 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'dbstrapi'),
-        username: env('DATABASE_USERNAME', 'dbuser'),
-        password: env('DATABASE_PASSWORD', 'test241'),
-        ssl: env.bool('DATABASE_SSL', false),
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'development') {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'dbstrapi'),
+            username: env('DATABASE_USERNAME', 'dbuser'),
+            password: env('DATABASE_PASSWORD', 'test241'),
+            ssl: env.bool('DATABASE_SSL', false),
+          },
+          options: {}
+        },
       },
-      options: {}
-    },
-  },
-});
+    }
+  } else {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'mysql',
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', ''),
+            username: env('DATABASE_USERNAME', ''),
+            password: env('DATABASE_PASSWORD', ''),
+            ssl: env.bool('DATABASE_SSL', false),
+          },
+          options: {}
+        },
+      },
+    }
+  }
+};
