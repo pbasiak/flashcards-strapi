73c6fca6141d4da923afa22c9e6af1e028963563
pbasiak <wilczek241@gmail.com>
2020-11-12 13:07:17 +0100

    update database

--- config/database.js
+++ config/database.js
@@ -26,8 +26,8 @@ module.exports = ({ env }) => {
           connector: 'bookshelf',
           settings: {
             client: 'mysql',
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 3306),
             database: env('DATABASE_NAME', ''),
             username: env('DATABASE_USERNAME', ''),
             password: env('DATABASE_PASSWORD', ''),
