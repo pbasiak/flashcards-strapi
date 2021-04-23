d68926215f608206f9c262a72db459d5ebc34fc1
pbasiak <wilczek241@gmail.com>
2021-04-23 20:28:31 +0200

    fix: update database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,23 @@
 module.exports = ({ env }) => {
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'mysql',
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('PROD_DATABASE_PORT', 3306),
+          database: env('PROD_DATABASE_NAME', ''),
+          username: env('PROD_DATABASE_USERNAME', ''),
+          password: env('PROD_DATABASE_PASSWORD', ''),
+          ssl: env.bool('PROD_DATABASE_SSL', true),
+        },
+        options: {}
+      },
+    },
+  }
+  
   if (env('NODE_ENV') === 'development') {
     return {
       defaultConnection: 'default',
