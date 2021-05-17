a762e2e2c561eee5b16350eb7c680b0885571618
pbasiak <wilczek241@gmail.com>
2021-05-17 22:38:50 +0200

    feat(flashcard): add author field

--- api/decks/controllers/decks.js
+++ api/decks/controllers/decks.js
@@ -14,11 +14,9 @@ module.exports = {
     if (ctx.is('multipart')) {
       const { data, files } = parseMultipartData(ctx);
       data.author = ctx.state.user.id;
-      console.log(data);
       entity = await strapi.services.decks.create(data, { files });
     } else {
       ctx.request.body.author = ctx.state.user.id;
-      console.log(ctx.request.body);
       entity = await strapi.services.decks.create(ctx.request.body);
     }
     return sanitizeEntity(entity, { model: strapi.models.decks });
--- api/flashcard/controllers/flashcard.js
+++ api/flashcard/controllers/flashcard.js
@@ -1,8 +1,56 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.author = ctx.state.user.id;
+      entity = await strapi.services.flashcard.create(data, { files });
+    } else {
+      ctx.request.body.author = ctx.state.user.id;
+      entity = await strapi.services.flashcard.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.flashcard });
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [flashcard] = await strapi.services.flashcard.find({
+      id: ctx.params.id,
+      'author.id': ctx.state.user.id,
+    });
+
+    if (!flashcard) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.flashcard.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.flashcard.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.flashcard });
+  },
+};
